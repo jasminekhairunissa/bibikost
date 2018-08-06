3532b474eae96942d6d709584f023120725ad1d6
Jasmine Khairunissa <khairunissajasmine@gmail.com>
2018-08-06 17:53:49 +0700

    display
    aboutus.blade, carikosan.blade, home.blade, kerjasama.blade, login.blade

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('kerjasama', function () {
+    return view('kerjasama');
+});
+
+Route::get('carikosan', function () {
+    return view('carikosan');
 });
