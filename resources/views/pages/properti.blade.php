88c26bfbfed917768c5228f7ddcaeae437c90738
jasminekhairunissa <khairunissajasmine@gmail.com>
2018-08-15 21:56:36 +0700

    page properti

---
+++ resources/views/pages/properti.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.defaultpemilik')
+@section('content')
+<div class="card-deck">
+  <div class="containercard">
+    <div class="card" style="width:300px">
+      <img class="card-img-top" src="kos.jpg" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h4 class="card-title">Kosan 1</h4>
+        <p class="card-text">lalalalalallalalalalalalaalalalalla</p>
+        <a href="#" class="btn btn-primary">Detail</a>
+        <a href="#" class="btn btn-danger">Edit</a>
+      </div>
+    </div>
+    <br>
+  </div>
+  <div class="containercard">
+    <div class="card" style="width:300px">
+      <img class="card-img-top" src="kos.jpg" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h4 class="card-title">Kosan 2</h4>
+        <p class="card-text">lalalalalallalalalalalalaalalalalla</p>
+        <a href="#" class="btn btn-primary">Detail</a>
+        <a href="#" class="btn btn-danger">Edit</a>
+      </div>
+    </div>
+    <br>
+  </div>
+  <div class="containercard">
+    <div class="card" style="width:300px">
+      <img class="card-img-top" src="add.png" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h4 class="card-title">Nama/Tipe Kosan Anda</h4>
+        <p class="card-text">lalalalalallalalalalalalaalalalalla</p>
+        <a href="#" class="btn btn-success">Tambah</a>
+      </div>
+    </div>
+    <br>
+  </div>
+
+</div>
+@stop
