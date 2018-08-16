235cb18a48d7cf9b62d40ebdf01c54183ebde614
jasminekhairunissa <khairunissajasmine@gmail.com>
2018-08-16 13:39:03 +0700

    bs-4 layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.default')
 @section('content')
-  <div class="bg"></div>
-  <div class="logo">
-    <img src="bikost.png" alt="Bibi Kost" width="400" height="400"></img>
+<div class="container h-100">
+  <div class="row h-100 justify-content-center align-items-center">
+      <div class="col">
+
+      </div>
+      <div class="col">
+        <div class="logo">
+          <img src="bikost.png" alt="Bibi Kost" width="100%" height="100%"></img>
+        </div>
+      </div>
+      <div class="col">
+
+      </div>
   </div>
-  <div class="signin">
-    <a href="{{( route('register.pemilik'))}}"><button class="button">Daftar Sekarang!</button>
+  <div class="row h-100 justify-content-center align-items-center">
+    <div class="col">
+
+    </div>
+    <div class="col">
+      <div class="signin">
+        <a href="{{( route('register.pemilik'))}}"><button class="button">Daftar Sekarang!</button>
+      </div>
+    </div>
+    <div class="col">
+
+    </div>
   </div>
+</div>
 @stop
