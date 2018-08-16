fda07fecd33e2efbf3e203d8ae22ece8f8577e7f
jasminekhairunissa <khairunissajasmine@gmail.com>
2018-08-16 13:41:31 +0700

    bs-4 layout

--- resources/views/pages/loginpemilik.blade.php
+++ resources/views/pages/loginpemilik.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.default')
 @section('content')
-<div class="card2">
-<div class="container">
-  <h2>Masuk sebagai Pemilik</h2>
-  <form action="/action_page.php">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-    </div>
-    <div class="checkbox">
-      <label><input type="checkbox" name="remember"> Remember me</label>
-    </div>
-    <button name="submitbtn" value="pencari" type="submit" class="btn btn-default">Login sebagai pencari</button>
-    <button name="submitbtn" value="pemilik" type="submit" class="btn btn-default">Login sebagai pemilik</button>
-  </form>
-</div>
+<div class="container h-100">
+  <div class="row h-100 justify-content-center align-items-center">
+    <form class="col-8">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      </div>
+      <div class="checkbox">
+        <label><input type="checkbox" name="remember"> Remember me</label>
+      </div>
+      <button class="buttonlogreg" name="submitbtn" value="pencari" type="submit" class="btn btn-default">Login sebagai Pencari</button>
+      <button class="buttonlogprem" name="submitbtn" value="pemilik" type="submit" class="btn btn-default">Login sebagai Pemilik</button>
+    </form>
+  </div>
 </div>
 @stop
